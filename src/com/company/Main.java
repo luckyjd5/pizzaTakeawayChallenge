8a6d28fe89bee8ef7e47f2b9cfac3d48ce357973
luckyjd5 <JD210029@reigate.ac.uk>
2021-11-08 15:24:37 +0000

    Made it more efficient. It uses 12 variables and uses less than 90 lines. Also added comments.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,16 @@ import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-        String[] sizeNTotal = sizeChoice();
-        String[] orderDetails = orderToppings(Double.parseDouble(sizeNTotal[1]));
-        if (orderDetails[0].length() > 0) {
+        String[] sizeNTotal = sizeChoice(); // Calls method which asks for size of pizza.
+        String[] orderDetails = orderToppings(Double.parseDouble(sizeNTotal[1])); // Calls method which asks for pizza toppings, inputting order total
+        if (orderDetails[0].length() > 0) { // If they order toppings it will display the toppings they ordered
             System.out.println("\nYou have ordered a " + sizeNTotal[0] + " pizza with:" + orderDetails[0] + "\nYour order costs £" + orderDetails[1]);
-        } else {
+        } else { // If they didn't order toppings, this will be displayed
             System.out.println("\nYou have ordered a " + sizeNTotal[0] + " pizza that costs £" + orderDetails[1]);
         }
     }
 
-    static String strInput() {
+    static String strInput() { // An input scanner method that allows inputs from methods without a scanner object, saves on space
         Scanner input = new Scanner(System.in);
         return input.nextLine();
     }
@@ -25,7 +25,7 @@ public class Main {
         double total = 0.00;
         String size = null;
         String[] orderDetails = {"", ""};
-        while (total == 0) {
+        while (total == 0) { // While loop stops invalid inputs from being processed
             System.out.println("Choose between a small, medium or large pizza:");
             size = strInput().toLowerCase();
             switch (size) {
@@ -44,11 +44,11 @@ public class Main {
         }
         orderDetails[0] = size;
         orderDetails[1] = Double.toString(total);
-        return orderDetails;
+        return orderDetails; // An array is returned because two variables need to be used when printing the order details
     }
 
     static String[] orderToppings(double total) {
-        int freeToppings = 2;
+        int freeToppings = 2; // The user has 2 free toppings, when this reaches 0 they must pay for toppings
         boolean errorRun;
         boolean end = false;
         String[] toppings = {"cheese", "ham", "pepperoni", "egg", "olives", "chicken", "bacon"};
@@ -57,12 +57,12 @@ public class Main {
         while (true) {
             total *= 100;
             total = Math.round(total);
-            total /= 100;
+            total /= 100; // Prevents precision errors in doubles and keeps the double to 2dp
             errorRun = true;
             System.out.println("\nYour order is £" + total + "\nEnter a topping:");
             String topping = strInput();
             topping = topping.toLowerCase();
-            for (String i : toppings) {
+            for (String i : toppings) { // If the inputted topping exists in the topping array, the topping is added to a list of toppings and if needed money is added to the total
                 if (topping.equals(toppings[Arrays.asList(toppings).indexOf(i)])) {
                     errorRun = false;
                     toppingsNTotal[0] += "• " + topping.toLowerCase() + "\n";
@@ -72,18 +72,18 @@ public class Main {
                         total += 1.25;
                     }
                 }
-                else if (topping.equalsIgnoreCase("end")) {
+                else if (topping.equalsIgnoreCase("end")) { // If the user enters 'end', they will end the while and for loops, taking them to the order details printing
                     end = true;
                     break;
                 }
             }
             if (end) {
                 break;
-            } else if (errorRun) {
+            } else if (errorRun) { // If the user's input does not match 'end' or any of the toppings in the toppings array, they go back to the beginning of the while loop
                 System.out.println("We do not offer that topping\nPlease enter a topping we do offer");
             }
         }
         toppingsNTotal[1] = Double.toString(total);
-        return toppingsNTotal;
+        return toppingsNTotal; // An array is returned because 2 variables are needed for the order details, toppings ordered and total price.
     }
 }
